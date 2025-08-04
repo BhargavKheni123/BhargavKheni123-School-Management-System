869205d767c6709c0ae8ec8b4c14a325c2c58c6d
vaidehipatel066 <vaidehipatel066@gmail.com>
2025-08-04 10:11:49 +0530

    mapper

---
+++ digital/Models/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using digital.Models;
+using digital.ViewModels;
+
+namespace digital.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Category, CategoryViewModel>().ReverseMap();
+        }
+    }
+}
